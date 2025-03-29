b057cf17330cb4cfcb6df38cb2bc30646e8409d3
Kota-704 <kktt14590358@gmail.com>
2025-03-30 01:03:23 +0900

    ✨ 新機能追加

--- diary/urls.py
+++ diary/urls.py
@@ -7,4 +7,7 @@ app_name = "diary"
 urlpatterns = [
   path("", views.index, name="index"),
   path("page/create/", views.page_create, name="page_create"),
+  path("pages/", views.page_list, name="page_list"),
+  path("pages/<uuid:id>/", views.page_detail, name="page_detail"),
+  path("pages/<uuid:id>/update", views.page_update, name="page_update"),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from datetime import datetime
 from zoneinfo import ZoneInfo
 from .forms import PageForm
+from .models import Page
+import os
+from django.conf import settings
 
 
 
@@ -19,12 +22,42 @@ class PageCreateView(View):
      return render(request, "diary/page_form.html", {"form": form})
 
   def post(self, request):
-    form = PageForm(request.POST)
+    form = PageForm(request.POST, request.FILES)
     if form.is_valid():
       form.save()
       return redirect("diary:index")
     return render(request, "diary/page_form.html", {"form" : form})
 
+class PageUpdateView(View):
+  def get(self, request, id):
+     page = get_object_or_404(Page, id=id)
+     form = PageForm(instance=page)
+     return render(request, "diary/page_update.html", {"form": form})
+
+  def post(self, request, id):
+    page = get_object_or_404(Page, id=id)
+    old_image = page.picture.path if page.picture else None
+    form = PageForm(request.POST, request.FILES, instance=page)
+    if form.is_valid():
+      if 'picture' in request.FILES and old_image and os.path.exists(old_image):
+        os.remove(old_image)
+      form.save()
+      return redirect("diary:page_detail", id=id)
+    return render(request, "diary/page_form.html", {"form" : form})
+
+class PageListView(View):
+  def get(self, request):
+    page_list = Page.objects.order_by("page_date")
+    return render(request, "diary/page_list.html", {"page_list": page_list})
+
+class PageDetailView(View):
+  def get(self, request, id):
+    page = get_object_or_404(Page, id=id)
+    return render(request, "diary/page_detail.html", {"page": page})
+
 
 index = IndexView.as_view()
 page_create = PageCreateView.as_view()
+page_list = PageListView.as_view()
+page_detail = PageDetailView.as_view()
+page_update = PageUpdateView.as_view()
